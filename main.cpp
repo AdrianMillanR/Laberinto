cdc6e1a7991f8fb6c33e2df6328a70177e1b46a5
Adrian Millan <adrian231998@gmail.com>
2020-09-09 12:53:12 -0500

    Creamos un arreglo unidimencional y posicionamos a nuesto heroe

--- main.cpp
+++ main.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+
+void dibujar_mapa(int PosicionHeroe,char mapa[9]){
+    for(int i=0;i<9;i++){
+        if(PosicionHeroe==i){
+            cout<<"H";
+        }else{
+            cout<<mapa[i];
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    char mapa[9];
+    for(int i=0;i<sizeof(mapa);i++){
+        mapa[i]='1';
+    }
+    int PosicionHeroe=6;
+
+    dibujar_mapa(PosicionHeroe,mapa);
+    /*for(int i=0;i<sizeof(mapa);i++){
+        if(PosicionHeroe==i){
+            cout<<"H";
+        }else{
+            cout<<mapa[i];
+        }
+    }*/
     return 0;
 }

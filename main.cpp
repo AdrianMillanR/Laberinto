c3f840553e6b45e63027bf9b1ac07294c387a905
Adrian Millan <adrian231998@gmail.com>
2020-09-09 13:29:44 -0500

    Nuestro Heroe ya se mueve en el mapa donde el tamaño lo define el usuario

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+int tamano=0;
+int PosicionHeroe=0;
+bool GameOver=false;
 
-void dibujar_mapa(int PosicionHeroe,char mapa[9]){
-    for(int i=0;i<9;i++){
+void dibujar_mapa(){
+    char mapa[tamano];
+    for(int i=0;i<sizeof(mapa);i++){
+        mapa[i]='1';
+    }
+    for(int i=0;i<sizeof(mapa);i++){
         if(PosicionHeroe==i){
             cout<<"H";
         }else{
@@ -15,19 +22,22 @@ void dibujar_mapa(int PosicionHeroe,char mapa[9]){
 
 int main()
 {
-    char mapa[9];
-    for(int i=0;i<sizeof(mapa);i++){
-        mapa[i]='1';
-    }
-    int PosicionHeroe=6;
+    char movimiento=' ';
+    cout<<"Bienvenido al laberinto interactivo en c++"<<endl;
+    cout<<"Para comenzar coloca de que tamano deseas que sea tu laberinto: ";
+    cin>>tamano;
 
-    dibujar_mapa(PosicionHeroe,mapa);
-    /*for(int i=0;i<sizeof(mapa);i++){
-        if(PosicionHeroe==i){
-            cout<<"H";
-        }else{
-            cout<<mapa[i];
+    dibujar_mapa();
+    while(GameOver==false){
+        cin>>movimiento;
+        if(movimiento=='a'){
+            PosicionHeroe-=1;
+        }else if(movimiento=='d'){
+            PosicionHeroe+=1;
+        }else if(movimiento=='p'){
+            GameOver=true;
         }
-    }*/
+        dibujar_mapa();
+    }
     return 0;
 }

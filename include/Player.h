2aa3773cc8c74ba6b7aecde11fc5ec556b9658ad
Adrian Millan <adrian231998@gmail.com>
2020-09-14 18:29:29 -0500

    Recreamos el proyecto desde cero, estructura basica en c++ y nuestra clase Player

---
+++ include/Player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+class Player
+{
+    public:
+        Player();
+        void Saludar();
+
+    protected:
+
+    private:
+};
+
+#endif // PLAYER_H
---
+++ src/Player.cpp
@@ -0,0 +1,11 @@
+#include "Player.h"
+#include <iostream>
+using namespace std;
+Player::Player()
+{
+    //ctor
+}
+
+void Player::Saludar(){
+    cout<<"Hola soy un heroe"<<endl;
+}
